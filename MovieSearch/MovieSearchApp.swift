68e2b2e8680aa87c33c2b80b2376892bd8dac693
John Cruz <johnamcruz@gmail.com>
2020-12-30 21:26:57 -1000

    Initial Commit

---
+++ MovieSearch/MovieSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearchApp.swift
+//  MovieSearch
+//
+//  Created by John M Cruz on 12/30/20.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
